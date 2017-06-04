891c0e420b7daa7dc145de32c11cd0147ab6aee5
Ignacio Hermosilla <hermosillavenegas@gmail.com>
2017-06-04 14:51:18 +0100

    fix(footer): fix updated video message (footer)

--- webmap/views.py
+++ webmap/views.py
@@ -88,11 +88,10 @@ def index(request):
     data = get_countries_info()
     existing_countries = map(str, data.keys())
     # @@TODO: handle empty database
-    updated_at = arrow.get(Country.objects.all()[0].updated_at).humanize()
     last_video = Country.get_latest_video_info()
     updated_info_template = '{updated_at} (latest added: {country} {video_type})'
     updated_info = updated_info_template.format(
-        updated_at=updated_at,
+        updated_at=last_video['video_added_at_humanized'],
         country=last_video['country'],
         video_type=last_video['video_type'],
     )
